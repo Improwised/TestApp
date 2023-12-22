1cd22f9dca088482504c8e1d0dcb558632a0d0ca
monikagorkhiya <monika@improwised.com>
2023-12-22 16:37:28 +0530

    feat: add common js and include into vue 3 application

---
+++ common/utils.js
@@ -0,0 +1,5 @@
+var utils
+export default utils = {
+  sum: function(value1,value2) { return parseFloat(value1) + parseFloat(value2); },
+  sub: function(value1,value2) { return parseFloat(value1) - parseFloat(value2); }
+}
