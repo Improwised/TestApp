42f3a04f1143d6fa53dfc7aef1e6f6986c95d2ad
Munir Khakhi <munir@improwised.com>
2023-12-26 10:52:05 +0530

    Used vite alias
    * Added eslint

--- web/vite.config.js
+++ web/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'common': fileURLToPath(new URL('../common', import.meta.url))
     }
   }
 })
