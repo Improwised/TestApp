224c4bbed846132b68ce9f3cb2ad9b3e8c86af9a
monikagorkhiya <monika@improwised.com>
2023-12-27 20:47:30 +0530

    fix: EOF line

--- common/utils.js
+++ common/utils.js
@@ -2,4 +2,3 @@ export default  {
   sum: function(value1,value2) { return parseFloat(value1) + parseFloat(value2); },
   sub: function(value1,value2) { return parseFloat(value1) - parseFloat(value2); }
 }
-
