41fcebae0898e4e259661666b99b1b5a1b81a8d8
Munir Khakhi <munir@improwised.com>
2023-12-29 11:16:03 +0530

    Fix: check for space infix

--- api/.eslintrc.cjs
+++ api/.eslintrc.cjs
@@ -38,6 +38,9 @@ module.exports = {
 		'semi': [
 			'error',
 			'never'
+		],
+		'space-infix-ops': [
+			'error'
 		]
 	}
 }
--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const utils = require('./common')
-const express= require('express')
+const express = require('express')
 
 const app = express()
 const port = 3000
