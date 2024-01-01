f3b97ceefbb12afe46f959fab3fbc639ead7d872
monikagorkhiya <monika@improwised.com>
2024-01-01 18:32:50 +0530

    add jsconfig.json file for working with alias path

--- common/subtract.js
+++ common/subtract.js
@@ -2,7 +2,6 @@
 * @param {number} value1 some number e.g 12
 * @param {number} value2 some number e.g 15
 * @type {number}
-* @typedef {import('../../common') } of subtract method
 * @returns subtract of two number
 */
 const subtract = (value1, value2) => parseFloat(value1) - parseFloat(value2);
