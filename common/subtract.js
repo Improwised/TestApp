dc06a0d53bc687b1c8a9e296ab0332d57a093431
Monika Gorkhiya <97655102+monikagorkhiya@users.noreply.github.com>
2024-01-01 17:50:03 +0530

    feat: add jsdoc wise comments (#11)

--- common/subtract.js
+++ common/subtract.js
@@ -1,3 +1,9 @@
-// math function subtract contain two argument, it should be number, returns subtract of two number
+/** Math function subtract contain two argument, it should be number, returns subtract of two number
+* @param {number} value1 some number e.g 12
+* @param {number} value2 some number e.g 15
+* @type {number}
+* @typedef {import('../../common') } of subtract method
+* @returns subtract of two number
+*/
 const subtract = (value1, value2) => parseFloat(value1) - parseFloat(value2);
 module.exports = subtract;
